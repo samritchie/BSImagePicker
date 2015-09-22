4f583fdb4ce16b7ce39da3ae67a5a4823f40aa38
Sam Ritchie <sam@codesplice.com.au>
2015-09-22 11:53:37 +0800

    Restructured project, removed cocoapods, added dynamic framework

---
+++ BSImagePicker.h
@@ -0,0 +1,18 @@
+//
+//  BSImagePicker.h
+//  BSImagePicker
+//
+//  Created by Sam Ritchie on 22/09/2015.
+//  Copyright © 2015 codesplice pty ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BSImagePicker.
+FOUNDATION_EXPORT double BSImagePickerVersionNumber;
+
+//! Project version string for BSImagePicker.
+FOUNDATION_EXPORT const unsigned char BSImagePickerVersionString[];
+
+#import "UIImageViewModeScaleAspect.h"
+
